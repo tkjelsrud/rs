395ca01074892f673484f4251775536376719a7f
Thomas Kjelsrud <tkjelsrud@Thomass-MacBook-Pro.local>
2016-09-05 20:13:57 +0200

    .

--- read-also.py
+++ read-also.py
@@ -5,6 +5,11 @@
 __author__      = "Barack Obama"
 __copyright__   = "Copyright 2009, Planet Earth"
 
+#Actebis_SKU	Mfr_SKU	Mfr_Name	Product_Group	Product_Text	Product_Description	
+#Price		Stock_Qty	Stock_Date	Barcode	TaxKey
+#
+
+
 try:
 	f = open('also_10606786.txt', 'r')
 	hLine = True
